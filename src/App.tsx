0cb0b9a42def23ea17a56c803ffe202adb3e455c
Diego Abdo <diegoabdov@gmail.com>
2026-06-08 13:57:40 -0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+        Vite + React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
